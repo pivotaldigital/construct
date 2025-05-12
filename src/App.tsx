ae6b5dd5fab3f73e3ed7dfd02d59b899fb6b55d3
Pivotal Digital <78040406+pivotaldigital@users.noreply.github.com>
2025-05-12 13:34:40 +0100

    Update package.json dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,14 @@
 import React from 'react';
-import { Stack, useTheme } from '@mui/material';
-
-import InspectorDrawer, { INSPECTOR_DRAWER_WIDTH } from './App/InspectorDrawer';
-import SamplesDrawer, { SAMPLES_DRAWER_WIDTH } from './App/SamplesDrawer';
-import TemplatePanel from './App/TemplatePanel';
-import { useInspectorDrawerOpen, useSamplesDrawerOpen } from './documents/editor/EditorContext';
-
-function useDrawerTransition(cssProperty: 'margin-left' | 'margin-right', open: boolean) {
-  const { transitions } = useTheme();
-  return transitions.create(cssProperty, {
-    easing: !open ? transitions.easing.sharp : transitions.easing.easeOut,
-    duration: !open ? transitions.duration.leavingScreen : transitions.duration.enteringScreen,
-  });
-}
+import { Box } from '@mui/material';
+import { Reader } from '@usewaypoint/email-builder';
+import { useDocument } from './documents/editor/EditorContext';
 
 export default function App() {
-  const inspectorDrawerOpen = useInspectorDrawerOpen();
-  const samplesDrawerOpen = useSamplesDrawerOpen();
-
-  const marginLeftTransition = useDrawerTransition('margin-left', samplesDrawerOpen);
-  const marginRightTransition = useDrawerTransition('margin-right', inspectorDrawerOpen);
+  const document = useDocument();
 
   return (
-    <>
-      <InspectorDrawer />
-      <SamplesDrawer />
-
-      <Stack
-        sx={{
-          marginRight: inspectorDrawerOpen ? `${INSPECTOR_DRAWER_WIDTH}px` : 0,
-          marginLeft: samplesDrawerOpen ? `${SAMPLES_DRAWER_WIDTH}px` : 0,
-          transition: [marginLeftTransition, marginRightTransition].join(', '),
-        }}
-      >
-        <TemplatePanel />
-      </Stack>
-    </>
+    <Box sx={{ p: 2 }}>
+      <Reader document={document} rootBlockId="root" />
+    </Box>
   );
 }
