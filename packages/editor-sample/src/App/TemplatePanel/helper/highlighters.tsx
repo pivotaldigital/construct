763c84db40f7d4e0d0edf8150a80a1540c59abea
Pivotal Digital <78040406+pivotaldigital@users.noreply.github.com>
2025-05-12 11:03:51 +0100

    Fix highlight.js imports

--- packages/editor-sample/src/App/TemplatePanel/helper/highlighters.tsx
+++ packages/editor-sample/src/App/TemplatePanel/helper/highlighters.tsx
@@ -1,11 +1,13 @@
 import hljs from 'highlight.js';
+import jsonHighlighter from 'highlight.js/lib/languages/json';
+import xmlHighlighter from 'highlight.js/lib/languages/xml';
 import prettierPluginBabel from 'prettier/plugins/babel';
 import prettierPluginEstree from 'prettier/plugins/estree';
 import prettierPluginHtml from 'prettier/plugins/html';
 import { format } from 'prettier/standalone';
 
-hljs.registerLanguage('json', require('highlight.js/lib/languages/json'));
-hljs.registerLanguage('html', require('highlight.js/lib/languages/xml'));
+hljs.registerLanguage('json', jsonHighlighter);
+hljs.registerLanguage('html', xmlHighlighter);
 
 export async function html(value: string): Promise<string> {
   const prettyValue = await format(value, {
