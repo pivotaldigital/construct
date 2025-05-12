ecb36f617f04bbf4c880db2ebad838c54976adba
Pivotal Digital <78040406+pivotaldigital@users.noreply.github.com>
2025-05-12 09:49:31 +0100

    Set up basic email builder

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+export default function App() {
+  return (
+    <Box sx={{ p: 3 }}>
+      <Typography variant="h4">Email Builder</Typography>
+    </Box>
+  );
+}
---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#f2f5f7',
+    },
+    text: {
+      primary: '#1F1F21',
+      secondary: '#4F4F4F',
+    }
+  }
+});
+
+export default theme;
